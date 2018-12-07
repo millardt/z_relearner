c73fc92b82b75541adfbc2720728aea7b5315e99
millardt <naglet@student.vvc.edu>
2018-12-06 17:29:44 -0800

    update

---
+++ cis-201-practice/ch-1/hello_world/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+	int main()
+{
+	cout << "Hello me This is TRZ";
+	return 0;
+}
---
+++ cis-201-practice/ch-2/coins/coins1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+double pennies = 0.01;
+double nickels = 0.05;
+double dimes = 0.1;
+
+double total = pennies *1 + dimes *3 + nickels *2; // total value of coins
+
+cout << "total value is " << total << endl;
+
+return 0;
+
+}
